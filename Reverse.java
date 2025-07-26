3d4f783d60be10f84461018b27106076fac1c45c
Sivaranjani1502 <sivasumi1523@gmail.com>
2025-07-26 12:33:17 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Reverse {
     public static void main(String[] args) {
        Scanner scan=new Scanner(System.in);
        int number=scan.nextInt();
