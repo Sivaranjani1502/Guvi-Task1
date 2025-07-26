22cf4a5de622ebc81dd9c996a051254a54deb49c
Sivaranjani1502 <sivasumi1523@gmail.com>
2025-07-26 12:04:02 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.lang.System;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       for(int i=10;i<=50;i++){
+           System.out.print(i + " ");
+       }
+    }
+}
